45488c56076913b7b011f7848ea87e0cfcc9996b
George Papagapitos <george.g.papagapitos@gmail.com>
2021-01-29 16:18:10 -0600

    created button functionality and employee object

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,20 @@ $(onReady);
 
 function onReady() {
   console.log('jq');
+  $(document).on('click', '#submit-button', onSubmit);
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log('submit');
+
+  const employee = {
+    firstName: $('#first-name-input').val(),
+    lastName: $('#last-name-input').val(),
+    id: $('#id-input').val(),
+    title: $('#title-input').val(),
+    annualSalary: Number($('#salary-input').val()),
+  };
+
+  console.log(employee);
 }
